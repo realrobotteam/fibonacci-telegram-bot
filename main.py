b7c54082f12c4d3077ae1462511fce1630dbaa94
Real Robot | رئال ربات <112397575+realrobotteam@users.noreply.github.com>
2025-05-06 02:00:39 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ from handlers import (
     start, gemini_stream_handler, gemini_pro_stream_handler, clear, switch,
     gemini_private_handler, gemini_photo_handler, gemini_edit_handler, draw_handler,
     handle_channel_membership, handle_assistant_callback, get_assistants_markup,
-    get_content_menu_markup, handle_content_callback, handle_content_text, get_special_tools_markup, handle_special_tools_callback,
-    handle_assistant_text
+    get_content_menu_markup, handle_content_callback, handle_content_text, get_special_tools_markup, handle_special_tools_callback
 )
 from channel_checker import check_membership, get_join_channel_markup, CHANNEL_ID
 
@@ -63,13 +62,6 @@ async def main():
     bot.register_message_handler(
         gemini_private_handler,
         func=lambda message: message.chat.type == "private" and (message.from_user.id not in handlers.user_content_state or not handlers.user_content_state[message.from_user.id]['type'].startswith('assistant_')),
-        content_types=['text'],
-        pass_bot=True)
-    
-    # Register assistant text handler
-    bot.register_message_handler(
-        handle_assistant_text,
-        func=lambda message: message.chat.type == "private" and message.from_user.id in handlers.user_content_state and handlers.user_content_state[message.from_user.id]['type'].startswith('assistant_'),
         content_types=['text'],
         pass_bot=True)
     
